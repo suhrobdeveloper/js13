afd7dd664af38c24c94e96d497aacb37d0c3fcf0
suhrobdeveloper <abdullahilyasoglutr@gmail.com>
2023-11-05 21:03:37 +0500

    1

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
-var countDownDate = new Date("Nov 22, 2023 15:37:25").getTime();
+let cdDate = new Date("Nov 22, 2023 15:37:25").getTime();
 
-var x = setInterval(function () {
-  var now = new Date().getTime();
+let  x = setInterval(function () {
+  let now = new Date().getTime();
 
   
-  var distance = countDownDate - now;
+  let distance = cdDate - now;
 
  
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   
   document.getElementById("demo").innerHTML =
